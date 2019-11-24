7562f233198f472bed41f6d758320a6df83a9846
Rojas Cubas <jrojascu@unprg.edu.pe>
2019-11-24 15:47:25 -0500

    archivo2

---
+++ lote.err.bat
@@ -0,0 +1,44 @@
+python rojascubas/mientras/ejercicio1.py -10 20 10 07
+python rojascubas/mientras/ejercicio2.py juana 1555 ebchebc
+python rojascubas/mientras/ejercicio3.py -5 -2 4
+python rojascubas/mientras/ejercicio4.py -10 2 -4
+python rojascubas/mientras/ejercicio5.py juerves 31 junnio 1200
+python rojascubas/para/ejercicio6.py -7
+python rojascubas/para/ejercicio7.py 18
+python rojascubas/para/ejercicio8.py -2
+python rojascubas/para/ejercicio9.py -20
+python rojascubas/para/ejercicio10.py 0 0
+python rojascubas/iteracion/ejercicio11.py 12345
+python rojascubas/iteracion/ejercicio12.py 423456789
+python rojascubas/iteracion/ejercicio13.py -4651
+python rojascubas/iteracion/ejercicio14.py 15112hola
+python rojascubas/iteracion/ejercicio15.py ""
+python rojascubas/iteracionenrango/ejercicio16.py -5
+python rojascubas/iteracionenrango/ejercicio17.py -13 10
+
+python LIZA_DAMIAN_CARLOS/MIENTRAS/bucle_mientras01.py "Frank" 25 10 15
+python LIZA_DAMIAN_CARLOS/MIENTRAS/bucle_mientras02.py -10 -8 50
+python LIZA_DAMIAN_CARLOS/MIENTRAS/bucle_mientras03.py -8 15 -1 20
+python LIZA_DAMIAN_CARLOS/MIENTRAS/bucle_mientras04.py -21 17 4
+python LIZA_DAMIAN_CARLOS/MIENTRAS/bucle_mientras05.py 97 3 38
+
+python LIZA_DAMIAN_CARLOS/PARA/bucle_para01.py -25
+python LIZA_DAMIAN_CARLOS\PARA\bucle_para02.py -21
+python LIZA_DAMIAN_CARLOS\PARA\bucle_para03.py &&&
+python LIZA_DAMIAN_CARLOS\PARA\bucle_para04.py -8
+python LIZA_DAMIAN_CARLOS\PARA\bucle_para05.py &&
+
+python LIZA_DAMIAN_CARLOS\ITERACION\bucle_iteracion01.py &&
+python LIZA_DAMIAN_CARLOS\ITERACION\bucle_iteracion02.py &&&
+python LIZA_DAMIAN_CARLOS\ITERACION\bucle_iteracion03.py OE
+python LIZA_DAMIAN_CARLOS\ITERACION\bucle_iteracion04.py &
+python LIZA_DAMIAN_CARLOS\ITERACION\bucle_iteracion05.py &
+
+python LIZA_DAMIAN_CARLOS\ITERACION_RANGO\bucle_iteracion_rango01.py -15
+python LIZA_DAMIAN_CARLOS\ITERACION_RANGO\bucle_iteracion_rango02.py a
+python LIZA_DAMIAN_CARLOS\ITERACION_RANGO\bucle_iteracion_rango03.py &&
+python LIZA_DAMIAN_CARLOS\ITERACION_RANGO\bucle_iteracion_rango04.py D
+python LIZA_DAMIAN_CARLOS\ITERACION_RANGO\bucle_iteracion_rango05.py 8P
+
+
+
