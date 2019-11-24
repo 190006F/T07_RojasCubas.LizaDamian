a618d54ece16ba1b1e4035e9ae757fb3013b8f22
Rojas Cubas <jrojascu@unprg.edu.pe>
2019-11-24 15:47:11 -0500

    archivo1

---
+++ lote.bat
@@ -0,0 +1,42 @@
+python rojascubas/mientras/ejercicio1.py 10 20 10 07
+python rojascubas/mientras/ejercicio2.py juan 12345F 1234567
+python rojascubas/mientras/ejercicio3.py 14 15 16
+python rojascubas/mientras/ejercicio4.py 5 10 2
+python rojascubas/mientras/ejercicio5.py miercoles 15 julio 2016
+python rojascubas/para/ejercicio6.py 22
+python rojascubas/para/ejercicio7.py  21
+python rojascubas/para/ejercicio8.py  15
+python rojascubas/para/ejercicio9.py  14
+python rojascubas/para/ejercicio10.py  -10 10
+python rojascubas/iteracion/ejercicio11.py hola bienbenido
+python rojascubas/iteracion/ejercicio12.py 94547214
+python rojascubas/iteracion/ejercicio13.py 14521
+python rojascubas/iteracion/ejercicio14.py  hola.comoestas....
+python rojascubas/iteracion/ejercicio15.py  progra12345ciclo
+python rojascubas/iteracionenrango/ejercicio16.py 5
+python rojascubas/iteracionenrango/ejercicio17.py -5 5
+
+python LIZA_DAMIAN_CARLOS/MIENTRAS/bucle_mientras01.py "Carlos" 42 10 15
+python LIZA_DAMIAN_CARLOS/MIENTRAS/bucle_mientras02.py 50 10 150
+python LIZA_DAMIAN_CARLOS/MIENTRAS/bucle_mientras03.py 20 15 18 20
+python LIZA_DAMIAN_CARLOS/MIENTRAS/bucle_mientras04.py 52 200 13
+python LIZA_DAMIAN_CARLOS/MIENTRAS/bucle_mientras05.py 88 23 154
+
+python LIZA_DAMIAN_CARLOS/PARA/bucle_para01.py 15
+python LIZA_DAMIAN_CARLOS\PARA\bucle_para02.py 28
+python LIZA_DAMIAN_CARLOS\PARA\bucle_para03.py "HOLA MUNDO"
+python LIZA_DAMIAN_CARLOS\PARA\bucle_para04.py 19
+python LIZA_DAMIAN_CARLOS\PARA\bucle_para05.py "PROGRAMACION"
+
+python LIZA_DAMIAN_CARLOS\ITERACION\bucle_iteracion01.py "FERNANDO"
+python LIZA_DAMIAN_CARLOS\ITERACION\bucle_iteracion02.py "ESTEFANY"
+python LIZA_DAMIAN_CARLOS\ITERACION\bucle_iteracion03.py 8
+python LIZA_DAMIAN_CARLOS\ITERACION\bucle_iteracion04.py "BUCLES"
+python LIZA_DAMIAN_CARLOS\ITERACION\bucle_iteracion05.py "REPETITIVAS"
+
+
+python LIZA_DAMIAN_CARLOS\ITERACION_RANGO\bucle_iteracion_rango01.py 14
+python LIZA_DAMIAN_CARLOS\ITERACION_RANGO\bucle_iteracion_rango02.py 8
+python LIZA_DAMIAN_CARLOS\ITERACION_RANGO\bucle_iteracion_rango03.py "MATEMATICA II"
+python LIZA_DAMIAN_CARLOS\ITERACION_RANGO\bucle_iteracion_rango04.py 4
+python LIZA_DAMIAN_CARLOS\ITERACION_RANGO\bucle_iteracion_rango05.py 10
